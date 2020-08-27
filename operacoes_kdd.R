58b6ce83b039372793ec8f8e98449f78edc7bc24
Wesley Maffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2020-08-26 22:29:34 -0300

    First Commit

---
+++ operacoes_kdd.R
@@ -0,0 +1,5 @@
+#### ATIVIDADE IAA006 - ETAPA PRÉ-PROCESSAMENTO KDD
+### DATASET: BRAZILIAN HOUSES TO RENT
+## https://www.kaggle.com/rubenssjr/brasilian-houses-to-rent?select=houses_to_rent_v2.csv
+
+
