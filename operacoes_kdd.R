be0043a29c452bdc8a73f0cfc1835e84c19e96dc
Wesley Maffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2020-08-31 22:17:05 -0300

    DATASET BASELINE

--- operacoes_kdd.R
+++ operacoes_kdd.R
@@ -33,50 +33,100 @@
 #### BEGINNING #########################
 ########################################
 
+install.packages("tidyverse")
+install.packages("e1071")
+install.packages("randomForest")
+install.packages("kernlab")
+install.packages("caret")
+
+library("e1071")
+library("randomForest")
+library("kernlab")
+library("caret")
+library("dbplyr")
+
 # 0 - Personal Environment set (Diretório padrão pessoal para salvar outputs do R)
 getwd()
 setwd("C:/Users/wesle/OneDrive - ufpr.br/IAA006 - Arquitetura de Dados/Dataset/iaa006-brazil-apartment-renting-regression-model")
-
+set.seed(7)
 
 # 1 - Dataset loading
 houses <- read.csv("houses_to_rent_v2.csv")
+colnames(houses)[1] <- "cidade"
+colnames(houses)[3] <- "comodos"
+colnames(houses)[4] <- "banheiros"
+colnames(houses)[5] <- "vagas"
+colnames(houses)[6] <- "andares"
+colnames(houses)[7] <- "animais"
+colnames(houses)[8] <- "mobilia"
+colnames(houses)[9] <- "condominio"
+colnames(houses)[10] <- "aluguel"
+colnames(houses)[11] <- "iptu"
+colnames(houses)[12] <- "seguro"
+colnames(houses)[13] <- "somatorio"
 
-View(houses)
-
-colnames(houses)
+houses$cod <- 1:nrow(houses)
+houses_old <- houses
 
 summary(houses)
 
-
-# 2 - Data Partition/Selection
-library("caret")
-
-index <- createDataPartition(houses$total..R.., p = 0.80, list = FALSE)
-train_partition <- houses[index, ]
-test_partition <- houses[-index, ]
+View(houses)
 
 
-# 3 - Model Trainning
-statistics_model <- lm(houses$total..R.. ~ area + rooms + bathroom + parking.spaces + floor
-                       + animal_new + furniture_new + hoa..R.. + rent.amount..R.. + property.tax..R.. 
-                       + fire.insurance..R.. + total..R.., data = train_partition)
-# rf <- train(VOL~., data = treino, method = "rf")
-# svm <- train(VOL~., data = treino, method = "svmRadial")
-# rna <- train(VOL~., data = treino, method = "nnet", trace = FALSE)
+#### TRATAMENTOS MÍNIMOS PARA RODAR MODELO ######
+###############################
+# 2 - Coluna Cidades
+houses$cidade_new <- as.factor(houses$cidade)
 
+# 3 - Coluna Andares
+houses[houses$andares == "-", which(colnames(houses)=="andares")] <- "0"
+houses$andares_new <- as.numeric(houses$andares)
 
-# 3 - Data adjusts
+# 4 - Coluna Animais
+houses$animais_new <- ifelse(houses$animais == "acept", 1, 0)
+
+# 5 - Coluna Mobilia
+houses$mobilia_new <- ifelse(houses$mobilia == "furnished", 1, 0)
+
+# 6 - Fatiar 15% da base total para reduzir o número de registros para treino (senão leva mto tempo pro algoritmo)
+indices_total <- createDataPartition(houses$cidade, p=0.15, list=FALSE)
+houses_fatiado <- houses[indices_total, ]
+
+# 7 - Fatiar 80% para treino e 20% para teste
+indices <- createDataPartition(houses_fatiado$cidade, p=0.80, list=FALSE)
+
+treino <- houses_fatiado[indices,]
+
+teste <- houses_fatiado[-indices,]
+
+# 8 - Treinar um modelo Random Forest com a base de treino (coloquei um SVMRadial para teste e como exemplo)
 set.seed(7)
 
-row_numbers <- nrow(houses)
-houses$cod <- 1:row_numbers
+modelo_treino_rf <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+                            aluguel + iptu + seguro, data = treino, method = "rf")
 
-houses$furniture_new <- ifelse(houses$furniture == "furnished", 1, 0)
-houses$animal_new <- ifelse(houses$animal == "acept", 1, 0)
+# modelo_treino_svm <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+#                      aluguel + iptu + seguro, method = "svmRadial")
 
-houses$floor_new <- ifelse(houses$floor == "-", 0, houses$floor)
-houses$floor_new <- as.numeric(as.character(houses$floor_new))
+# modelo_treino_rna <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+#                      aluguel + iptu + seguro, data = treino, method = "nnet", trace = FALSE)
 
-houses
+# 9 - Usar o modelo treinado, anteriormente, agora com a base de teste para testar a classificação
+modelo_predito_rf <- predict(modelo_treino_rf, teste)
+# modelo_predito_svm <- predict(modelo_treino_svm, teste)
+# modelo_predito_rna <- predict(modelo_treino_rna, teste)
+
+# 10 - Matriz de confusão para visualizar resultado da classificação com 
+confusionMatrix(modelo_predito_rf, teste$cidade_new)
+# confusionMatrix(modelo_predito_svm, teste$cidade)
+# confusionMatrix(modelo_predito_rna, teste$cidade)
 
 
+#####################################################################
+#### IMPROVEMENTS START HERE - LET'S DO IT! #########################
+#####################################################################
+
+# ... remover outliers, min, max, registros nada a ver, tudo o que não prestar
+
+# sua função amiga:
+summary(houses_fatiado)
--- testes_wesley.R
+++ testes_wesley.R
@@ -1,13 +1,19 @@
-- Cenário inicial vs. Cenário final
-- Documento, o que você espera encontrar escrito?
-- Formato padrão que vc mandou está diferente do publicado no RITA
+install.packages("tidyverse")
+install.packages("e1071")
+install.packages("randomForest")
+install.packages("kernlab")
+install.packages("caret")
 
-# install.packages("tidyverse")
+library("e1071")
+library("randomForest")
+library("kernlab")
+library("caret")
 library("dbplyr")
 
 # 0 - Personal Environment set (Diretório padrão pessoal para salvar outputs do R)
 getwd()
 setwd("C:/Users/wesle/OneDrive - ufpr.br/IAA006 - Arquitetura de Dados/Dataset/iaa006-brazil-apartment-renting-regression-model")
+set.seed(7)
 
 # 1 - Dataset loading
 houses <- read.csv("houses_to_rent_v2.csv")
@@ -23,10 +29,70 @@ colnames(houses)[10] <- "aluguel"
 colnames(houses)[11] <- "iptu"
 colnames(houses)[12] <- "seguro"
 colnames(houses)[13] <- "somatorio"
-set.seed(7)
+
 houses$cod <- 1:nrow(houses)
+houses_old <- houses
 
 summary(houses)
+
+View(houses)
+
+
+#### TRATAMENTOS MÍNIMOS PARA RODAR MODELO ######
+###############################
+# 2 - Coluna Cidades
+houses$cidade_new <- as.factor(houses$cidade)
+
+# 3 - Coluna Andares
+houses[houses$andares == "-", which(colnames(houses)=="andares")] <- "0"
+houses$andares_new <- as.numeric(houses$andares)
+
+# 4 - Coluna Animais
+houses$animais_new <- ifelse(houses$animais == "acept", 1, 0)
+
+# 5 - Coluna Mobilia
+houses$mobilia_new <- ifelse(houses$mobilia == "furnished", 1, 0)
+
+# 6 - Fatiar 15% da base total para reduzir o número de registros para treino (senão leva mto tempo pro algoritmo)
+indices_total <- createDataPartition(houses$cidade, p=0.15, list=FALSE)
+houses_fatiado <- houses[indices_total, ]
+
+# 7 - Fatiar 80% para treino e 20% para teste
+indices <- createDataPartition(houses_fatiado$cidade, p=0.80, list=FALSE)
+
+treino <- houses_fatiado[indices,]
+
+teste <- houses_fatiado[-indices,]
+
+# 8 - Treinar um modelo Random Forest com a base de treino (coloquei um SVMRadial para teste e como exemplo)
+set.seed(7)
+
+modelo_treino_rf <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+                   aluguel + iptu + seguro, data = treino, method = "rf")
+
+# modelo_treino_svm <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+#                      aluguel + iptu + seguro, method = "svmRadial")
+
+# modelo_treino_rna <- train(cidade_new ~ area + comodos + banheiros + vagas + andares_new + animais_new + mobilia_new + condominio +
+#                      aluguel + iptu + seguro, data = treino, method = "nnet", trace = FALSE)
+
+# 9 - Usar o modelo treinado, anteriormente, agora com a base de teste para testar a classificação
+modelo_predito_rf <- predict(modelo_treino_rf, teste)
+# modelo_predito_svm <- predict(modelo_treino_svm, teste)
+# modelo_predito_rna <- predict(modelo_treino_rna, teste)
+
+# 10 - Matriz de confusão para visualizar resultado da classificação com 
+confusionMatrix(modelo_predito_rf, teste$cidade_new)
+# confusionMatrix(modelo_predito_svm, teste$cidade)
+# confusionMatrix(modelo_predito_rna, teste$cidade)
+
+
+
+
+
+
+
+
 
 # 2 -  Trocando andares para números e removendo valores grotescos de andares (Ex: 301)
 unique(houses$andares) # retorna tipos únicos de andares
@@ -39,12 +105,9 @@ houses$andares_new <- as.numeric(houses$andares)
 
 
 # 3 - Corrigindo cidade São Paulo que está com caractere não UTF-8
-houses_old <- houses
-
 unique(houses$cidade)
 
 houses[houses$cidade == "SÃ£o Paulo", which(colnames(houses) == "cidade")] = "São Paulo"
-
 
 # 4 - Trocando cidades para números
 houses$cidade_new <- as.numeric(as.factor(houses$cidade))
@@ -56,6 +119,36 @@ houses <- houses[houses$iptu != "313700", ]
 
 # 6 - Removendo Condomínio muito caro = "1117000"
 houses <- houses[houses$condominio != "1117000", ]
+houses <- houses[houses$condominio != "220000", ]
+#houses[houses$condominio == "200000", ]
+
+# 7 - Removendo apartamento com áreas distorcidas em m2
+# Exemplo custo área 2000m2 em BH: https://www.vivareal.com.br/imovel/predio-comercial-luxemburgo-bairros-belo-horizonte-2000m2-aluguel-RS85000-id-1041286922/
+houses <- houses[houses$area != "46335", ]
+houses <- houses[houses$area != "24606", ]
+houses <- houses[houses$area != "12732", ]
+houses <- houses[houses$area != "2000", ]
+
+
+
+houses[houses$aluguel == 500, ]
+
+
+houses[order(-houses$area), ]
+
+
+summary(houses)
+
+
+View(houses)
+
+
+
+
+
+
+
+
 
 # 5 - Estimando modelo preliminar anterior com CIDADES transformadas em números - R-QUADRADO = 0.9858
 model <- lm (aluguel ~ cidade_new + area + comodos + banheiros + vagas + andares + animais + mobilia + 
